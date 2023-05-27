98c0b6c0ec94517f50279fb668efcb2067c2f8f3
anhtk20012 <anhtk20012@gmail.com>
2023-05-27 15:31:17 +0700

    bài 1

---
+++ Buoi_1/buoi_1.py
@@ -0,0 +1,16 @@
+print("Họ và tên: Lã Trọng Ánh")
+print(5 + 6)
+# 2 x 1 = 2
+print("2 x 1 =", 2 * 1)
+print("2 x 2 =", 2 * 2)
+# Số mũ -> comment
+print("2^3 =", pow(2,3))
+
+"""
+Bài tập: Hiển thị bảng cửu chương 2,3,4,5
+Thực hiện tính bài 10110101011011011011101110(2) -> (10)
+"""
+# Enter
+print(pow);
+print("10 1101 0101 1011 0110 1110 1110")
+#1 2 3 5 6 7 9 10 12 13 15 16 18 20 22 23 25
