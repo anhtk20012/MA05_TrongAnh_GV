641488d192da50a7066cddc12b5463e381b56ec7
anhtk20012 <anhtk20012@gmail.com>
2023-06-24 13:43:11 +0700

    Checkpoint 1

--- Buoi_4/Buoi_4.py
+++ Buoi_4/Buoi_4.py
@@ -20,9 +20,34 @@ else:
     print("Bỏ vào giỏ D")
 '''
 
-n = float(input("Nhập n: "))
+'''n = float(input("Nhập n: "))
 
 if (n > 0):
     print("Trị tuyệt đối của", n, "là:", round(n))
 else: 
-    print("Trị tuyệt đối của", n , "là:", round(n * (-1)))
+    print("Trị tuyệt đối của", n , "là:", round(n * (-1)))'''
+
+'''n = int(input("Nhập một số n: "))
+if (n % 2 == 0):
+    print("N là số chẵn.")
+
+qua = int(input("Nhập quả cần bỏ vào giỏ: "))
+if (qua == 1):
+    print("Bỏ vào giỏ A")
+elif (qua == 2):
+    print("Bỏ vào giỏ B")
+elif (qua == 3):
+    print("Bỏ vào giỏ C")
+else:
+    print("Bỏ vào giỏ D")'''
+    
+Ten = input("Nhập tên học sinh trong lớp: ")
+
+if (Ten == "Binh Minh"):
+    print("Thằng này là Bình Minh.")
+elif (Ten == "Minh Duc"):
+    print("Thằng này là Minh Đức.")
+elif (Ten == "Anh Tuan"):
+    print("Thằng này là Anh Tuấn.")
+else:
+    print("Thằng này không có trong lớp.")
